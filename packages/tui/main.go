148fb0e31759e914e9ef216d3fe86f502b1d7acd
Christian Toivola <c.toivola@gmail.com>
2025-07-06 16:24:32 -0400

    Adds version check

--- packages/tui/main.go
+++ packages/tui/main.go
@@ -1,13 +1,26 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"os"
 
 	"tddpro/internal/tui"
 )
 
+const version = "v0.1.0"
+
 func main() {
+	// Add --version and -v flag support
+	showVersion := false
+	flag.BoolVar(&showVersion, "version", false, "Print version and exit")
+	flag.BoolVar(&showVersion, "v", false, "Print version and exit (shorthand)")
+	flag.Parse()
+	if showVersion {
+		fmt.Println(version)
+		os.Exit(0)
+	}
+
 	apiURL := tui.LoadAPIURL()
 	if err := tui.Start(apiURL); err != nil {
 		fmt.Println("Error running program:", err)
